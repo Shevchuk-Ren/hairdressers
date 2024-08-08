7096688d4004d292af0356fd609703887fa6d7c0
victorianimchenko <77270393+Shevchuk-Ren@users.noreply.github.com>
2024-08-08 02:09:55 +0100

    gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,23 +1,19 @@
-const slides = document.querySelectorAll('.carousel-slide');
-let currentIndex = 0;
+let slideIndex = 0;
+showSlides();
 
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.classList.remove('active');
-    if (i === index) {
-      slide.classList.add('active');
-    }
-  });
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active-slide", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active-slide";
+  setTimeout(showSlides, 2000); 
 }
-
-document.querySelector('.carousel-prev').addEventListener('click', () => {
-  currentIndex = (currentIndex === 0) ? slides.length - 1 : currentIndex - 1;
-  showSlide(currentIndex);
-});
-
-document.querySelector('.carousel-next').addEventListener('click', () => {
-  currentIndex = (currentIndex === slides.length - 1) ? 0 : currentIndex + 1;
-  showSlide(currentIndex);
-});
-
-showSlide(currentIndex);
